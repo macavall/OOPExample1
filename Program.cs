3022be552a0f1fa5fcf9bd0ef059607bebb9aaeb
Matthew Cavallo <macavall@microsoft.com>
2024-03-11 12:21:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var myClass = new MyClass();
+
+            Console.WriteLine("Writing out the values for MyClassProperty and MyClassField in that order: ");
+
+            myClass.MyClassProperty = "Hello from MyClassProperty!";
+            myClass.MyClassField = "Hello from MyClassField!";
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Executing MyClass Methods: ");
+
+            myClass.MyClassMethod();
+            myClass.MyClassMethodFieldProperty();
+
+            Console.ReadLine();
+        }
+
+        public class MyClass
+        {
+            public string? MyClassProperty { get; set; }
+
+            public string? MyClassField;
+
+            public void MyClassMethod()
+            {
+                Console.WriteLine("Hello from MyClassMethod!");
+            }
+
+            // write to console property and field
+            public void MyClassMethodFieldProperty()
+            {
+                Console.WriteLine($"MyClass Property value is: {MyClassProperty}");
+                Console.WriteLine($"MyClass Field value is: {MyClassField}");
+            }
+        }
+    }
+}
